dcbfb949fe61d8b1073fb404cd357dc9bc871b67
RS//6 <kheir93@hotmail.com>
2020-11-14 16:29:59 +0100

    banch_discover

--- script.js
+++ script.js
@@ -3,18 +3,30 @@ let info = document.querySelector(".info");
 let profile = document.querySelector(".profile");
 let editopen = document.querySelector(".info__edit-button");
 let form = document.querySelector(".form");
-
-
+let save = document.querySelector(".form__save");
 
 
 function popup() {
-
-  root.classList.add("root__overlay");
   form.classList.add("form_action_popup");
-  return form;
+  root.classList.add("root__overlay");
 };
-console.log(popup);
 
 editopen.addEventListener("click", popup);
 
 
+
+function handleFormSubmit(evt) {
+  evt.preventDefault();
+
+  let nameInput = document.querySelector('.form__name[value="Jacques Cousteau"}');
+  let jobInput = document.querySelector(".form__about");
+  let infoName = document.querySelector(".info__name");
+  let infoabout = document.querySelector(".info__about");
+
+  nameInput.textContent(infoName);
+  jobInput.textContent(infoabout);
+
+};
+
+console.log(handleFormSubmit)
+save.addEventListener('submit', handleFormSubmit);
